bb3b510c36f755d674086a255376fb63a138b022
Muthu <muthuvijayakumar5@gmail.com>
2018-11-14 09:31:03 +0530

    Initial onee

---
+++ Assets/BouncyJelly.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyJelly : MonoBehaviour
+{
+    public static BouncyJelly bouncy;
+
+    public GameObject[] Bones;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public Mesh cubeMesh;
+
+    public Vector3 centre;
+    public Vector3 temp;
+
+    public void Awake()
+    {
+        bouncy = this;
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < Bones.Length; i++)
+        {
+            centre += Bones[i].transform.position;
+        }
+
+        centre = centre / Bones.Length;
+        transform.position = centre;
+        centre = Vector3.zero;
+        playerMeshCollider();
+
+    }
+
+    void playerMeshCollider()
+    {
+        skinnedMeshRenderer.BakeMesh(cubeMesh);
+        gameObject.GetComponent<MeshCollider>().sharedMesh = cubeMesh;
+
+    }
+
+}
