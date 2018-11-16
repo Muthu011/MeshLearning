64928647e8b2a9b6932f13f4d5852197c494ddbd
Muthu <muthuvijayakumar5@gmail.com>
2018-11-16 08:16:10 +0530

    Clock
    New clock system i developed

---
+++ Assets/scripts/Clock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform, minutesTransform, secondsTransform;
+    const float degreesPerHours = 30f;
+    public bool isContinuous=true;
+
+	void Update ()
+    {
+        if (isContinuous)
+        {
+            Continuous();
+        }
+        else {
+            Discrete();
+        }
+
+    }
+
+    public void Continuous()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursTransform.localRotation = Quaternion.Euler(0f,(float) time.TotalHours * degreesPerHours, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * 6f, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * 6f, 0f);
+    }
+    public void Discrete()
+    {
+        hoursTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour * degreesPerHours, 0f);
+        minutesTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour * 6f, 0f);
+        secondsTransform.localRotation = Quaternion.Euler(0f, DateTime.Now.Hour * 6f, 0f);
+    }
+}
