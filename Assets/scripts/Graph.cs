efe439b29b97d45fdc526399c11cef47fc983fca
Muthu <muthuvijayakumar5@gmail.com>
2018-11-19 07:58:12 +0530

    graph cube in progress

---
+++ Assets/scripts/Graph.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform Point;
+    public int i;
+    private void Awake()
+    {
+       for(i=0;i<10;i++)
+        {
+            Transform point1 = Instantiate(Point);
+            point1.localPosition = Vector3.right * i;
+        }
+       
+    }
+
+}
